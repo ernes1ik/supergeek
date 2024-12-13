1c97b7655d3ae1e81e082b4333cbeff817cee9ef
Ernest <ernest.evt@gmail.com>
2024-12-13 16:11:52 +0300

    Добавляю плейлисты

--- project.py
+++ project.py
@@ -1,63 +1,5 @@
-playlist_e = """
-Sunday 5:09
-Why Does My Heart Feel so Bad? 4.23
-Everlong 3.25
-To Let Myself Go
-Golden 2.56
-Daisuke 2.41
-Miami 3.31
-Chill Bill Lofi 2.05
-The Perfect Girl 1.48
-Resonance 3.32
-"""
-
-playlist_f = (
-    {"Free Bird": 9.08, "Enter Sandman": 5.31, "One": 7.45, "Sliver": 2.10, "Come as You Are": 3.45},
-    {"Thunderstruck": 4.53, "You Shook Me All Night Long": 3.29, "Everlong": 4.51, "My Hero": 4.02},
-)
-
-import random
-from datetime import timedelta
-from typing import Iterable, Any, Union
-
-
-def parse_playlist(playlist: str) -> list:
-    songs = []
-    for line in playlist.strip().split("\n"):
-        name, duration = line.rsplit(" ", 1)
-        songs.append((name, float(duration)))
-    return songs
-
-
-def get_duration(playlist: Iterable, n: int) -> Union[timedelta, str, float]:
-    if isinstance(playlist, str):
-        playlist = parse_playlist(playlist)
-    elif isinstance(playlist, tuple) and all(isinstance(i, dict) for i in playlist):
-        playlist = [(song['title'], song['duration']) for song in playlist]
-
-    if n > len(playlist):
-        return
-
-    chosen_songs = random.sample(playlist, n)
-
-    total_duration = sum(song[1] for song in chosen_songs)
-
-    return timedelta(hours=int(total_duration // 60), minutes=int(total_duration % 60))
-
-
-multi_line_playlist = """
-Why Does My Heart Feel so Bad? 4.23
-Everlong 3.25
-To Let Myself Go 4.40
-Golden 2.56
-Daisuke 2.41
-Miami 3.31
-Chill Bill Lofi 2.05
-The Perfect Girl 1.48
-Resonance 3.32
-"""
-
-dict_playlist = (
+playlist_e = (
+    {"title": "Sunday", "duration": 5.09},
     {"title": "Why Does My Heart Feel so Bad?", "duration": 4.23},
     {"title": "Everlong", "duration": 3.25},
     {"title": "To Let Myself Go", "duration": 4.40},
@@ -68,6 +10,14 @@ dict_playlist = (
     {"title": "The Perfect Girl", "duration": 1.48},
     {"title": "Resonance", "duration": 3.32},
 )
-
-print(get_duration(multi_line_playlist, 3))
-print(get_duration(dict_playlist, 3))
+playlist_f = (
+    {"title": "Free Bird", "duration": 9.08},
+    {"title": "Enter Sandman", "duration": 5.31},
+    {"title": "One", "duration": 7.45},
+    {"title": "Sliver", "duration": 2.10},
+    {"title": "Come as You Are", "duration": 3.45},
+    {"title": "Thunderstruck", "duration": 4.53},
+    {"title": "You Shook Me All Night Long", "duration": 3.29},
+    {"title": "Everlong", "duration": 4.51},
+    {"title": "My Hero", "duration": 4.02},
+)
